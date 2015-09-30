152d4d1d6c3dccfcf74d94b18efb2f3063c406ea
Kris Wall <kris@urbanscouter.com>
2015-09-30 00:48:17 -0500

    Part 6 - Controller as Syntax

---
+++ Part 6 - Controller as Syntax/directory.js
@@ -0,0 +1,18 @@
+angular.module('directoryApp', [])
+	.controller('directoryController', function() {
+		
+		var dirList = this;
+
+		dirList.list = [
+			{name:'Scott', age: 29},
+			{name:'Ross', age: 31},
+			{name:'Ben', age: 32},
+			{name:'Courtney', age: 29}
+		];
+
+		dirList.addPerson = function() {
+			dirList.list.push({name:dirList.name, age: dirList.age});
+			dirList.name = '';
+			dirList.age = 0;
+		};
+	});
